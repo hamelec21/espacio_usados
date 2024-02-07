f74e7e2f76bc6626e62faa5a0409afc7b4cbad1e
juan ramon <juanramoncornejo@gmail.com>
2024-02-07 15:44:46 -0300

    Se Crea Crud de Marcas

--- app/Livewire/Panel/Config/Marcas/ShowMarcas.php
+++ app/Livewire/Panel/Config/Marcas/ShowMarcas.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire\Panel\Config\Marcas;
 
+use App\Models\Marca;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowMarcas extends Component
 {
+
+    use WithPagination;
+    public $search;
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    protected $listeners = ['render' => 'render'];
+
+    public function destroy($id)
+    {
+      Marca::destroy($id);
+    }
+
     public function render()
     {
-        return view('livewire.panel.config.marcas.show-marcas');
+        $marcas= Marca::buscar($this->search)
+        ->orderBy('id','ASC')
+        ->paginate(10);
+        return view('livewire.panel.config.marcas.show-marcas',compact('marcas'));
     }
+
 }
