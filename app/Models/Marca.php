51a244a518734cb4f0eb97727e9abe56fb0e07cd
juan ramon <juanramoncornejo@gmail.com>
2024-02-07 14:32:14 -0300

    Se Crea modelos marca y modelo

---
+++ app/Models/Marca.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Marca extends Model
+{
+    use HasFactory;
+    protected $table = 'marcas';
+    protected $fillable = ['nombre'];
+    public function scopeBuscar($query, $buscar)
+     {
+         if ($buscar === '') {
+             return;
+         }
+         return $query->where('nombre', 'like', '%' . $buscar . '%');
+     }
+}
---
+++ app/Models/Modelo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Modelo extends Model
+{
+    use HasFactory;
+    protected $table = 'modelos';
+    protected $fillable = ['nombre'];
+    public function scopeBuscar($query, $buscar)
+     {
+         if ($buscar === '') {
+             return;
+         }
+         return $query->where('nombre', 'like', '%' . $buscar . '%');
+     }
+}
