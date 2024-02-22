6acc5d36285040623ca1857e121991c37ed1aa07
juan ramon <juanramoncornejo@gmail.com>
2024-02-22 15:46:41 -0300

    Se crea fk categorias_id

---
+++ database/migrations/2024_02_22_182447_add_subcategorias_id_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('productos', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('subcategorias_id');
+            $table->foreign('subcategorias_id')->references('id')->on('subcategorias')
+            ->onDelete('no action')
+            ->onUpdate('no action');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('productos', function (Blueprint $table) {
+            //
+        });
+    }
+};
