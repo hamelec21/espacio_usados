586744667854d1266a928b3d14223db203823980
juan ramon <juanramoncornejo@gmail.com>
2024-02-23 22:26:58 -0300

    se crea sidebar

--- resources/views/livewire/vendedor/dashboard.blade.php
+++ resources/views/livewire/vendedor/dashboard.blade.php
@@ -1,3 +1,11 @@
 <div>
-    @livewire('vendedor.ventas.informes')
+    @include('navigation-menu')
+    @livewire('menu.vendedor-sidebar')
+
+     <div class="p-4 sm:ml-64">
+         <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+             @livewire('vendedor.ventas.informes')
+
+         </div>
+     </div>
 </div>
